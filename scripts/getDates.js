9647aafaacc41634838a9df49d3bedf033e16315
lauralizz <lauranrmn1@gmail.com>
2024-04-30 11:07:28 -0400

    Added Javascript

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,9 @@
-document.write("&copy; " + new Date().getFullYear());
-document.lastModified 
+function getCurrentYear() {
+    var currentDate = new Date();
+    return currentDate.getFullYear();
+}
+
+document.getElementById('currentDate').innerText += getCurrentYear();
+
+var lastModified = new Date(document.lastModified);
+document.getElementById('lastModified').innerText = lastModified;
