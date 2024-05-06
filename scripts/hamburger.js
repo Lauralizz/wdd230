9aa4ae059bd7a149bb8de1136253a89a39110695
lauralizz <lauranrmn1@gmail.com>
2024-05-05 20:23:13 -0400

    Team Activity

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,9 +1,25 @@
 // JavaScript - hamburger.js
-document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.getElementById('menu');
-    const nav = document.querySelector('.navigation');
 
-    menuBtn.addEventListener('click', function () {
-        nav.classList.toggle('open');
-    });
-});
+const hamburgerElement = document.querySelector('#menu');
+const navElement = document.querySelector('.navigation');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
+
+const modeButton = document.querySelector('#mode');
+const page = document.querySelector('html');
+//const dark = document.querySelector('#darkBtn');
+//const light = document.querySelector('#lightBtn');
+
+modeButton.addEventListener('click', () => {
+    if (modeButton.textContent.includes('dark')) {
+        page.style.background = '#474847';
+        modeButton.textContent = 'light';
+    } else {
+        page.style.background = 'white';
+        page.style.color = 'black'
+        modeButton.textContent = 'dark';
+    }
+})
