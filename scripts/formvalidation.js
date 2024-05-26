39041e8d142f37aab07d9faf77a98616df5a58a4
lauralizz <lauranrmn1@gmail.com>
2024-05-26 16:41:02 -0400

    fixed errors

--- scripts/formvalidation.js
+++ scripts/formvalidation.js
@@ -7,7 +7,7 @@ p2.addEventListener("focusout", checkSame);
 // This should be refactored.
 function checkSame() {
 	if (p1.value !== p2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.textContent = "❗Passwords DO NOT MATCH!";
 		message.style.visibility = "show";
 		p2.style.backgroundColor = "#fff0f3";
 		p2.value = "";
