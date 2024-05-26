175bd5187bc33fb39414480bad0ab1c2df6e2894
lauralizz <lauranrmn1@gmail.com>
2024-05-26 19:34:06 -0400

    form css

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -7,3 +7,13 @@ document.getElementById('currentDate').innerText += getCurrentYear();
 
 var lastModified = new Date(document.lastModified);
 document.getElementById('lastModified').innerText = lastModified;
+
+// Function to set the current date and time as the value of the hidden timestamp field
+function setTimestamp() {
+    var now = new Date();
+    var formattedDateTime = now.toISOString(); // Format date to ISO string
+    document.getElementById('timestamp').value = formattedDateTime;
+}
+
+// Call setTimestamp function when the window loads
+window.onload = setTimestamp;
