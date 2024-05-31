ecbc0d211247572787bc40c23af27f7470f57f53
lauralizz <lauranrmn1@gmail.com>
2024-05-30 21:42:38 -0400

    weather url

---
+++ week05/scripts/weather.js
@@ -0,0 +1,4 @@
+const currentTemp = document.querySelector('current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.749875320974&lon=6.639574085150954&appid=7f92890c6f54ee9b8db60b78ce26e38a';
