6155a5909a42d929b38f9dbfbe073359f010a832
lauralizz <lauranrmn1@gmail.com>
2024-05-30 22:26:27 -0400

    Finished weather page

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,4 +1,30 @@
-const currentTemp = document.querySelector('current-temp');
+const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.749875320974&lon=6.639574085150954&appid=7f92890c6f54ee9b8db60b78ce26e38a';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=7f92890c6f54ee9b8db60b78ce26e38a';
+
+async function apiFetch() {
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}  
