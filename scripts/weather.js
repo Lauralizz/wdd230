46af4aa52eb501b633a7fe59e8600ae93a6c9199
lauralizz <lauranrmn1@gmail.com>
2024-05-31 12:49:27 -0400

    weather on homepage

---
+++ scripts/weather.js
@@ -0,0 +1,30 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.63&lon=-84.16&units=imperial&appid=7f92890c6f54ee9b8db60b78ce26e38a';
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}  
