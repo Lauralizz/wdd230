ecc2933d7c61813629e593bf3b4851c0ce44435c
lauralizz <lauranrmn1@gmail.com>
2024-05-31 20:07:35 -0400

    Display JSON

---
+++ chamber/scripts/members.js
@@ -0,0 +1,36 @@
+const baseURL = "https://lauralizz.github.io/wdd230/chamber/";
+const linksURL = "https://lauralizz.github.io/wdd230/chamber/data/members.json";
+const membersSection = document.getElementById('membersContainer');
+
+async function getMembersData() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let businessName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let businessAddress = document.createElement('p');
+        let businessPhone = document.createElement('p');
+
+        businessName.textContent = `${member.name}`;
+        portrait.setAttribute('src', member.image);
+        businessAddress.textContent = `Address: ${member.address}`;
+        businessPhone.textContent = `Phone Number: ${member.phoneNumber}`;
+
+        card.appendChild(businessName);
+        card.appendChild(portrait);
+        card.appendChild(businessAddress);
+        card.appendChild(businessPhone);
+
+        membersSection.appendChild(card);
+    });
+}
+
+// Ensure the DOM is fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', (event) => {
+    getMembersData();
+});
